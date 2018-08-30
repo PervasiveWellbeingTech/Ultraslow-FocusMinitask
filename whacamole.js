bfa419b2bee968993b7ea9ebca300a6550460e2c
Manan Rai <mananrai@stanford.edu>
2018-08-30 14:00:36 -0700

    Changed mole movement mechanism

--- whacamole.js
+++ whacamole.js
@@ -7,17 +7,22 @@ window.addEventListener('load', update);
 var sheet = document.createElement('style');
 document.body.appendChild(sheet);
 
-var global_start = false;
+var global_start_allowed = false;
 
 let score = 0;
 var moleOnScreen = false;
 
 var reactionTimes = {
+    mole: [],
     click: [],
     hover: []
 }
 
 var startTime = 0;
+var moleTime = 0;
+var interval = 0;
+
+var game_timeout = undefined;
 
 // process update message
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -26,8 +31,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
-function getRandomTime() {
-    return Math.round(Math.random() * (max - min) + min);
+function getRandomTime(min, max) {
+    return (Math.random() * (max - min) + min);
 }
 
 function getRandomLocation() {
@@ -45,83 +50,100 @@ function getRandomLocation() {
     return location;
 }
 
-function addMole(location) {
-    // console.log(location);
-    var main_divs = document.body.querySelectorAll("#game"); // check if there's already a box
-    console.log(main_divs);
-    if (main_divs.length > 0) {
-        var hole_div = document.createElement("div"); 
-        hole_div.setAttribute("id", "hole");
-        hole_div.setAttribute('style',
-          // "background: url(" + chrome.extension.getURL('img/dirt.svg') + ") bottom center no-repeat; \n" +
-          "left: " + location.x + "px; \n" +
-          "top: " + location.y + "px;");
+function createMole() {
+    var hole_div = document.createElement("div"); 
+    hole_div.setAttribute("id", "hole");
+    hole_div.setAttribute("style", "display: none;");
+    // hole_div.setAttribute('style',
+    //   // "background: url(" + chrome.extension.getURL('img/dirt.svg') + ") bottom center no-repeat; \n" +
+    //   "left: " + location.x + "px; \n" +
+    //   "top: " + location.y + "px;");
 
-        sheet.innerHTML = sheet.innerHTML +
-                          "#hole:after { \n" +
-                          "display: block; \n" +
-                          "background: url(" + chrome.extension.getURL('img/dirt.svg') + ") bottom center no-repeat; \n" +
-                          "background-size: contain; \n" +
-                          "content: ''; \n" +
-                          "width: 100%; \n" +
-                          "height: 70px; \n" +
-                          "position: absolute; \n" +
-                          "z-index: 2; \n" +
-                          "bottom: -30px; \n" +
-                          "} \n";
+    sheet.innerHTML = sheet.innerHTML +
+                      "#hole:after { \n" +
+                      "display: block; \n" +
+                      "background: url(" + chrome.extension.getURL('img/dirt.svg') + ") bottom center no-repeat; \n" +
+                      "background-size: contain; \n" +
+                      "content: ''; \n" +
+                      "width: 100%; \n" +
+                      "height: 70px; \n" +
+                      "position: absolute; \n" +
+                      "z-index: 2; \n" +
+                      "bottom: -30px; \n" +
+                      "} \n";
 
-        document.getElementById("game").appendChild(hole_div);
+    document.getElementById("game").appendChild(hole_div);
 
-        var mole_div = document.createElement("div");
-        mole_div.setAttribute("id", "mole");
-        mole_div.setAttribute('style',
-          "background: url(" + chrome.extension.getURL('img/mole.svg') + ") bottom center no-repeat; \n" +
-          "background-size: 60%;"
-        );
-        // mole_div.addEventListener('click', moleClicked);
-        document.getElementById("hole").appendChild(mole_div);
+    var mole_div = document.createElement("div");
+    mole_div.setAttribute("id", "mole");
+    mole_div.setAttribute('style',
+      "background: url(" + chrome.extension.getURL('img/mole.svg') + ") bottom center no-repeat; \n" +
+      "background-size: 60%;"
+    );
+    // mole_div.addEventListener('click', moleClicked);
+    document.getElementById("hole").appendChild(mole_div);
 
-        document.getElementById("hole").addEventListener("mouseover", mouseReachedMole);
-        document.getElementById("hole").addEventListener("click", moleClicked);
+    document.getElementById("hole").addEventListener("mouseover", mouseReachedMole);
+    document.getElementById("hole").addEventListener("click", moleClicked);
 
-        moleOnScreen = true;
-    }
+    // moleOnScreen = true;
 }
 
-function mouseReachedMole() {
-    var eventTime = new Date().getTime();
-    reactionTimes.hover.push(eventTime - startTime);
-    startTime = eventTime;
-}
-
-function moleClicked() {
-    var eventTime = new Date().getTime();
-    reactionTimes.click.push(eventTime - startTime);
-    startTime = eventTime;
-
-    score++;
-    console.log("Clicked! Score now is " + score);
-    removeMole();
-}
-
-function nextMole() {
+function displayMole(location) {
     moleOnScreen = true;
-    var location = getRandomLocation();
-    console.log(location);
-    addMole(location);
+
+    var hole_div = document.getElementById("hole");
+    hole_div.setAttribute('style',
+        // "background: url(" + chrome.extension.getURL('img/dirt.svg') + ") bottom center no-repeat; \n" +
+        "display: block; \n" + 
+        "left: " + location.x + "px; \n" +
+        "top: " + location.y + "px;");
+    // hole_div.setAttribute("style", "display: block;");
+
+    startTime = new Date().getTime();
+    reactionTimes.mole.push(startTime);
 }
 
 function removeMole() {
     moleOnScreen = false;
     
-    var mole_div = document.getElementById("mole");
-    if (mole_div) {
-        mole_div.remove();
-    }
+    // var mole_div = document.getElementById("mole");
+    // if (mole_div) {
+    //     mole_div.remove();
+    // }
 
-    var hole_div = document.getElementById("hole");
-    if (hole_div) {
-        hole_div.remove();
+    // var hole_div = document.getElementById("hole");
+    // if (hole_div) {
+    //     hole_div.remove();
+    // }
+
+    document.getElementById("hole").setAttribute("style", "display: none;");
+}
+
+function nextMole() {
+    moleOnScreen = true;
+    var location = getRandomLocation();
+    console.log("Next mole location: " + location);
+    displayMole(location);
+}
+
+function mouseReachedMole() {
+    var eventTime = new Date().getTime();
+    if (reactionTimes.mole.length > reactionTimes.hover.length) {
+        reactionTimes.hover.push(eventTime - startTime);
+        // startTime = eventTime;
+    }
+}
+
+function moleClicked() {
+    var eventTime = new Date().getTime();
+    if (moleOnScreen && reactionTimes.mole.length > reactionTimes.click.length) {
+        reactionTimes.click.push(eventTime - startTime);
+        // startTime = eventTime;
+
+        score++;
+        console.log("Clicked! Score now is " + score);
+        removeMole();
     }
 }
 
@@ -131,7 +153,7 @@ function gamePlay(mole_time, interval) {
     //     setTimeout(() => {
     //         var location = getRandomLocation();
     //         console.log(location);
-    //         addMole(location);    
+    //         displayMole(location);    
     //         setTimeout(removeMole, time);
     //     }, time + 2000);
     // }, 60 * 1000);
@@ -148,7 +170,7 @@ function gamePlay(mole_time, interval) {
     //     }, time);
     // }, time + 2000);
 
-    var interval = setInterval(singleCycle, mole_time + interval);
+    game_interval = setInterval(singleCycle, mole_time + interval);
 
     function singleCycle() {
         nextMole();
@@ -167,23 +189,45 @@ function gamePlay(mole_time, interval) {
 }
 
 function randomGamePlay(mole_time, interval, total_time) {
-    var timeout = setTimeout(singleCycle, mole_time + interval + getRandomeTime(-5, 5));
+    game_timeout = setTimeout(singleCycle, mole_time + interval + getRandomTime(-5, 5));
 
     function singleCycle() {
         nextMole();
-        startTime = new Date().getTime();
+        // startTime = new Date().getTime();
 
         setTimeout(() => {
-          if (moleOnScreen) {
-            removeMole();
-          }
+            console.log("hello 1");
+            if (moleOnScreen) {
+                console.log("hello 2");
+                moleOnScreen = false;
+                if (reactionTimes.mole.length > reactionTimes.click.length) {
+                    console.log("hello 3");
+                    reactionTimes.click.push(-1);
+                }
+                if (reactionTimes.mole.length > reactionTimes.hover.length) {
+                    console.log("hello 4");
+                    reactionTimes.hover.push(-1);
+                }
+                removeMole();
+            }
         }, mole_time);
 
-        timeout = setTimeout(singleCycle, mole_time + interval + getRandomeTime(-5, 5));
-        if (score === 5) {
-            clearInterval(interval);
-        }
+        game_timeout = setTimeout(singleCycle, mole_time + interval + getRandomTime(-5, 5));
+        // if (score === 5) {
+        //     clearTimeout(game_timeout);
+        // }
     }
+}
+
+function stopPlay(mole_time, interval) {
+    console.log(game_timeout);
+    if (moleOnScreen) {
+        removeMole();
+    }
+    if (game_timeout) {
+        clearTimeout(game_timeout);
+    }
+    console.log(reactionTimes);
 }
 
 function clearAll() {
@@ -213,12 +257,22 @@ function clearAll() {
 }
 
 function keydownHandler(e) {
-    console.log(e)
-    // Accept enter, shift, ctrl, alt, and space
-    var acceptedKeys = [13, 16, 17, 18, 32]
-    if (acceptedKeys.includes(e.keyCode)) {
-        global_start = !global_start;
-        console.log(global_start);
+    console.log(e);
+    
+    // // Accept enter, shift, ctrl, alt, and space as start key(s)
+    // var acceptedStartKeys = [13, 16, 17, 18, 32];
+
+    // Accept enter and space as start key(s)
+    var acceptedStartKeys = [13, 32];
+    // Accept esc as end key(s)
+    var acceptedEndKeys = [27];
+    
+    if (acceptedStartKeys.includes(e.keyCode) && global_start_allowed) {
+        // global_start = !global_start;
+        randomGamePlay(mole_time, interval);
+    } else if (acceptedEndKeys.includes(e.keyCode) && global_start_allowed) {
+        console.log("Should be stopping");
+        stopPlay(mole_time, interval);
     }
 }
 
@@ -232,13 +286,7 @@ function update() {
         interval: 30,
         mole_time: 1.5
     }, function(items) {
-        clearAll();
-
         document.addEventListener('keydown', keydownHandler, false);
-
-        if (!items.enabled) {
-            return;
-        }
 
         // while (!global_start) {
         //     // Wait
@@ -247,25 +295,56 @@ function update() {
         console.log(items);
 
         var divs = document.body.querySelectorAll("#game"); // check if there's already a box
-        if (divs.length < 1 && items.enabled) {
+        var hole_divs = document.body.querySelectorAll("#hole");
+        var mole_divs = document.body.querySelectorAll("#mole");
+
+        if (!items.enabled) {
+            global_start_allowed = false;
+            if (divs.length > 0) {
+                var div = document.getElementById("game");
+                div.remove();
+            }
+            if (hole_divs.length > 0) {
+                var hole_div = document.getElementById("hole");
+                hole_div.remove();
+            }
+            if (mole_divs.length > 0) {
+                var mole_div = document.getElementById("mole");
+                mole_div.remove();
+            }
+            return;
+        }
+
+        if (divs.length < 1) {
             var div = document.createElement("div"); 
             div.setAttribute("id", "game"); 
             document.body.insertBefore(div, document.body.firstChild);
-        } else if (divs.length > 0 && !items.enabled) {
-            var div = document.getElementById("game"); 
-            div.remove();
         }
 
-        while (!global_start) {
-            // Wait
+        if (hole_divs.length < 1 && mole_divs.length < 1) {
+            createMole();
         }
 
-        var mole_time = (items.mole_time < 500) ? (items.mole_time * 1000) : items.mole_time
+        // while (!global_start) {
+        //     // Wait
+        //     console.log("Waiting")
+        // }
+
+        if (items.mole_time <= 0) {
+            items.mole_time = 30;
+        }
+
+        if (items.interval <= 0) {
+            items.interval = 1.5;
+        }
+
+        mole_time = (items.mole_time < 500) ? (items.mole_time * 1000) : items.mole_time
         console.log(mole_time);
-        var interval = (items.interval < 500) ? (items.interval * 1000) : items.interval
+        interval = (items.interval < 500) ? (items.interval * 1000) : items.interval
         console.log(interval);
         
-        gamePlay(mole_time, interval);
+        global_start_allowed = true;
+        // gamePlay(mole_time, interval);
     });
 }
 
